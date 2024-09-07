42ec5176855989a3cef49e53247ba84c558ed011
Gleb Surinov <glebovsky@gmail.com>
2024-09-07 13:51:46 +0600

    feat(navigation): add animation to navigation (wip)

--- src/components/organisms/Navigation/index.tsx
+++ src/components/organisms/Navigation/index.tsx
@@ -1,13 +1,44 @@
 'use client'
 
+import { motion } from 'framer-motion'
 import { Link } from '@chakra-ui/next-js'
-import { Flex, Stack, Button, useColorModeValue, useDisclosure } from '@chakra-ui/react'
-import Section from '@/components/molecules/Section'
-import Logo from '@/components/molecules/Logo'
-import MobileMenuButton from '@/components/organisms/Navigation/MobileMenuButton'
+import { Flex, Stack, Grid, Button, useColorModeValue, useDisclosure } from '@chakra-ui/react'
+import Section from '@/components/Section'
+import Logo from '@/components/Logo'
+import MobileMenuButton from '@/components/Navigation/MobileMenuButton'
 
 type TNavigationProps = {
   showLogoTitle?: boolean
+}
+
+const navigationMenuWrapperVariants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+}
+
+const sectionVariants = {
+  open: {
+    height: '100dvh',
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    height: 'auto',
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
 }
 
 const Navigation = ({ showLogoTitle = false }: TNavigationProps) => {
@@ -15,24 +46,41 @@ const Navigation = ({ showLogoTitle = false }: TNavigationProps) => {
   const bg = useColorModeValue('gray.50', 'gray.900')
 
   return (
-    <Section py="4" bg={bg} position="fixed" inset="0 0 auto 0" zIndex="sticky">
-      <Stack
-        direction={{ base: 'column', md: 'row' }}
-        align={{ base: 'stretch', md: 'center' }}
-        justify="space-between"
+    <Section
+      py="4"
+      bg={bg}
+      position="fixed"
+      inset="0 0 auto 0"
+      zIndex="sticky"
+      alignItems="flex-start"
+      as={motion.div}
+      variants={sectionVariants}
+      animate={isOpen ? 'open' : 'close'}
+    >
+      <Grid
+        gridTemplateColumns={{ base: '1fr', md: 'auto 1fr' }}
+        gridTemplateRows={{ base: 'auto 1fr', md: '1fr' }}
+        gap="4"
       >
         <Flex justify="space-between">
           <Logo showTitle={showLogoTitle} />
           <MobileMenuButton isOpen={isOpen} onToggle={onToggle} />
         </Flex>
-        <Stack as="nav" direction="row" align="center" gap="6" display={{ base: 'none', md: 'flex' }}>
+        <Stack
+          as="nav"
+          direction={{ base: 'column', md: 'row' }}
+          align={{ base: 'flex-start', md: 'center' }}
+          gap={{ base: '4', md: '6' }}
+          ml={{ base: '0', md: 'auto' }}
+          display="none"
+        >
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <Button as={Link} href="/contact" colorScheme="gray">
             Contact
           </Button>
         </Stack>
-      </Stack>
+      </Grid>
     </Section>
   )
 }
